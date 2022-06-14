121f990394d1a9fe0207c817778f772bf9618a92
rosareitmeir <ge72few@mytum.de>
2022-06-14 16:15:11 +0200

    preparing rush data + dge rush vs old/male

--- RNA-seq/dge_rush_main_data.R
+++ RNA-seq/dge_rush_main_data.R
@@ -1,12 +1,11 @@
-
+library(DESeq2)
 library(readr)
 library(stringr)
-
+library(dplyr)
 rush_data<- read.csv("/home/rosa/rush_data/all_counts.csv",  header=TRUE)
-row.names(rush_data)<- rush_data$hgnc_symbol
 rush_data<- rush_data[,2:ncol(rush_data)]
-
-rush_data <- rush_data %>% distinct(hgnc_symbol, .keep_all=TRUE )
+rush_data<- dplyr::select(rush_data, -c(ensembl_gene_id, gene_biotype))
+rownames(rush_data)<- rush_data$hgnc_symbol
 
 
 rnaseqdata <- read.table("/home/rosa/Dokumente/GSE153873_summary_count.star.txt", sep = "\t",header = TRUE,row.names = 1)
@@ -17,13 +16,94 @@ dim(rnaseqdata)
 
 
 # Groups
-AD <- select(rnaseqdata, starts_with("AD"))
-young <- select(rnaseqdata, starts_with("Young"))
-old <- select(rnaseqdata, starts_with("Old"))
+AD <- dplyr::select(rnaseqdata, starts_with("AD"))
+young <- dplyr::select(rnaseqdata, starts_with("Young"))
+old <- dplyr::select(rnaseqdata, starts_with("Old"))
+female<- dplyr::select(rush_data, -hgnc_symbol)
+
+old<- dplyr::select(old, -hgnc_symbol)
 
 
-old$hgnc_symbol <- rownames(old)
+# differential gene expression: control vs female
+# 23589 common measured genes 
 
-# 23603 common measured genes 
+control_female<- inner_join(old, rush_data,  by="hgnc_symbol")
+rownames(control_female)<- control_female$hgnc_symbol
+control_female<- dplyr::select(control_female, -hgnc_symbol)
 
-control_female<- inner_join(rush_data, old, by="hgnc_symbol")
+AD$hgnc_symbol<- rownames(AD)
+male_female<- inner_join(AD, rush_data,  by="hgnc_symbol")
+rownames(male_female)<- male_female$hgnc_symbol
+male_female<- dplyr::select(male_female, -hgnc_symbol)
+AD<-  dplyr::select(AD, -hgnc_symbol)
+
+
+execute_dge<- function(matrix, samplesize1, samplesize2){
+  condition <- factor(c(rep("G1",samplesize1),rep("G2",samplesize2)))
+  
+  dds_cf <- DESeqDataSetFromMatrix(countData = matrix,
+                                   DataFrame(condition), ~ condition)
+  dds_cf <- DESeq(dds_cf)
+  return (dds_cf)
+  
+}
+
+dds<- execute_dge(male_female, ncol(AD), ncol(female))
+res<- results(dds,alpha= 0.05)
+
+#421 genes with significant upregulation in AD, while 434
+# had significant downregulation -> 855 significant up/downregulated
+resOrdered <- res[order(res$pvalue),]
+summary(res)
+
+pvalues <- p.adjust(res$pvalue, method = "BH")
+sum(pvalues<0.05,na.rm = T)
+
+
+# MA plot
+plotMA(res, ylim=c(-4,4))
+
+resNorm <- lfcShrink(dds, coef=2, type="normal")
+plotMA(resNorm, ylim=c(-1.5,1.5),main='Normal')
+
+
+#434 downregulated, 421 upregulated
+plotdf<- data_frame(log2FoldChange=res$log2FoldChange, padj= res$padj,genes=res$row)
+
+plotdf<- plotdf %>%
+  mutate(gene_type = case_when(log2FoldChange >= 0.6999 & padj < 0.05 ~ "upregulated",
+                               log2FoldChange <= -0.668 & padj < 0.05 ~ "downregulated",
+                               TRUE ~ "not significant"))   
+# OR
+plotdf<- data_frame(log2FoldChange=res$log2FoldChange, padj= res$padj)
+plotdf<- plotdf %>%
+  mutate(gene_type = case_when(log2FoldChange > 0.661 & pvalues < 0.05 ~ "upregulated",
+                               log2FoldChange < -0.615 & pvalues < 0.05 ~ "downregulated",
+                               TRUE ~ "not significant"))   
+
+print(table(plotdf$gene_type))
+
+upregulated <- plotdf[plotdf$gene_type=="upregulated",]
+downregulated <- plotdf[plotdf$gene_type=="downregulated",]
+
+# Comparison to the paper's results
+sheets <- excel_sheets("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx")
+x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+resultupreg <- read_excel("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx",sheet = sheets[1])
+resultdownreg <- read_excel("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx",sheet = sheets[2])
+
+
+
+
+# Plot counts for a single gene -> smallest pvalue
+plotCounts(dds, gene=which.min(res$padj), intgroup="condition")
+
+#Heatmap of the count matrix + cluster
+ntd <- normTransform(dds)
+select <- order(rowMeans(counts(dds,normalized=TRUE)),
+                decreasing=TRUE)[1:15]
+pheatmap(assay(ntd)[select,])
+
+#Principal component plot of the samples
+
+plotPCA(vsd, intgroup=c("condition"))
---
+++ RushData/prepare_rushdata.R
@@ -0,0 +1,144 @@
+library(readr)
+library(dplyr)
+library('biomaRt')
+library(tximport)
+
+# info about samples rna seq data from rush , filtering out file name of the samples
+report<- read.csv("/home/rosa/Downloads/experiment_report_2022_6_10_10h_48m.tsv", sep="\t", header=TRUE)
+samples<- read.table("/home/rosa/rush_data/sample.txt", sep="\t", header=TRUE)
+filenames<- read.table("/home/rosa/rush_data/helpers/filenames.txt")
+
+
+report["file_name"]<- NA
+for (filename in filenames$V1){
+  for (i in 1:nrow(report)){
+    list<- report[i,15]
+    if (grepl(filename,list )){
+      report[i, ncol(report)] <- filename
+      
+    }
+  }
+  
+}
+reduced_report<- report[, c(1,2,17,40, 3,4,9,21,22,23, 18,19)]
+
+write_csv(reduced_report, "/home/rosa/rush_data/overview.tsv")
+
+
+
+dir<- "/home/rosa/rush_data"
+overview<- read.csv("/home/rosa/rush_data/helpers/overview.csv", header=TRUE)
+
+# combining all 29 rna seq data to one rsem object
+
+files <- file.path(dir, paste0(overview$file_name, ".tsv"))
+names(files) <-overview$Accession
+txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+
+# expected count table 
+rush_counts<- as.data.frame(txi.rsem$counts)
+
+# removing zero counts and ercc counts
+rush_counts<- rush_counts[650:nrow(rush_counts),]
+rush_counts<- rush_counts[1:58780,]
+#removing par_Y counts 
+rush_counts<- filter(rush_counts, !endsWith(rownames(rush_counts), "PAR_Y"))
+
+# remove endings of gene names 
+row.names(rush_counts)<- gsub("\\..*", "", rownames(rush_counts))
+
+# convert ensembl ids to gene symbols
+mart <- useMart(dataset="hsapiens_gene_ensembl","ensembl")
+G_list <- getBM(filters= "ensembl_gene_id", 
+                attributes=c( 'ensembl_gene_id',
+                              'hgnc_symbol', 'gene_biotype'),
+                values= rownames(rush_counts),
+                mart= mart)
+
+rush_counts$ensembl_gene_id <- rownames(rush_counts)
+
+merged<- left_join(rush_counts, G_list, by="ensembl_gene_id")
+
+
+
+# filter out  data set
+
+# all not protein coding without a gene symbol 
+not_protein_coding<- merged$ensembl_gene_id[which(merged$gene_biotype!= "protein_coding" & merged$hgnc_symbol == "")]
+only_ensembl_info<- merged$ensembl_gene_id[which(is.na(merged$gene_biotype) & is.na(merged$hgnc_symbol))]
+merged<- filter(merged, !(ensembl_gene_id %in% c(not_protein_coding, only_ensembl_info)))
+
+
+no_symbol_yet <- filter(merged, hgnc_symbol =="")
+
+G_list <- getBM(filters= "ensembl_gene_id", 
+                attributes=c( 'refseq_mrna', 'ensembl_gene_id',
+                              'hgnc_symbol', 'gene_biotype'),
+                values= no_symbol_yet$ensembl_gene_id,
+                mart= mart)
+
+
+
+# remove all rows without gene symbol and no  refseq id
+no_info<- G_list$ensembl_gene_id[which(G_list$refseq_mrna == "")]
+merged<- filter(merged, !(ensembl_gene_id %in% no_info))
+
+
+
+no_symbol_yet<- filter(G_list, refseq_mrna != "")
+
+# obtained gene symbol with help of resfseq id using website: .... 
+refseq_genesym<- read.csv("/home/rosa/rush_data/refseq_genesymbol.tsv", header=FALSE)
+colnames(refseq_genesym)<- c("refseq_mrna", "hgnc_symbol")
+
+now_symbol_yet <- left_join(no_symbol_yet, refseq_genesym, by="refseq_mrna")
+
+# remove all rows without symbols
+
+no_symbol <- filter(now_symbol_yet, hgnc_symbol.y == "")$ensembl_gene_id
+merged<- nrow(filter(merged, !(ensembl_gene_id %in% no_symbol)))
+
+
+
+now_symbol_yet <- filter(now_symbol_yet, hgnc_symbol.y != "")
+now_symbol_yet<- dplyr::select(now_symbol_yet, -c( hgnc_symbol.y))
+now_symbol_yet<- unique(now_symbol_yet)
+
+# add now gene symbol to the ensembl ids 
+
+positions<- which(merged$ensembl_gene_id %in% now_symbol_yet$ensembl_gene_id)
+
+helper<-data.frame(ensembl_gene_id= merged$ensembl_gene_id[positions]  ,
+                   row_num=positions)
+
+now_symbol_yet<- filter(left_join(now_symbol_yet, helper, by="ensembl_gene_id"), !is.na(row_num))
+now_symbol_yet<- now_symbol_yet[order(now_symbol_yet$row_num),]
+
+merged[which(merged$ensembl_gene_id %in% now_symbol_yet$ensembl_gene_id), "hgnc_symbol"] <- now_symbol_yet$hgnc_symbol
+
+# now every ensembl id has a gene symbol 
+
+# get rid of ensembl ids with same gene symbol 
+
+occur<- as.data.frame(table(merged$hgnc_symbol,useNA = 'always'))
+
+duplicates_hgnc <- filter( occur, Freq >1)
+
+
+merged<- filter(merged, !( hgnc_symbol %in% duplicates_hgnc$Var1))
+
+
+
+merged<- merged  %>% dplyr::select(ensembl_gene_id, hgnc_symbol, gene_biotype,   everything())
+
+
+merged[, 4:ncol(merged)]<- round(merged[, 4:ncol(merged)])
+
+write.csv(file="/home/rosa/rush_data/all_counts.csv", merged)
+
+
+
+
+
+
+
