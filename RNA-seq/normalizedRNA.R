f615399e4ebdce4a7cdcdf275c55542200cb7b3c
ingazalia <98822685+ingazalia@users.noreply.github.com>
2022-06-16 23:27:13 +0200

    normalized read counts per kb

---
+++ RNA-seq/normalizedRNA.R
@@ -0,0 +1,32 @@
+library(stringr)
+library(DESeq2)
+library(EDASeq)
+rnaseqdata <- read.table("RNAseq/GSE153873_summary_count.star.txt", sep = "\t",header = TRUE,row.names = 1)
+colnames(rnaseqdata) <- paste0(data.frame(str_split(colnames(rnaseqdata),'[.]'))[3,],data.frame(str_split(colnames(rnaseqdata),'[.]'))[1,])
+
+
+# get entrez id
+library('org.Hs.eg.db')
+ensembl <- mapIds(org.Hs.eg.db, rownames(rnaseqdata), 'ENSEMBL', 'SYMBOL')
+ensemblnew <- ensembl[!is.na(ensembl)]
+genelength <- getGeneLengthAndGCContent(id=ensemblnew,'hsa')
+getGeneLengthAndGCContent(id=entrez[11],'hsa')
+
+condition <- factor(colnames(rnaseqdata))
+deseqrna <- DESeqDataSetFromMatrix(rnaseqdata[which(!is.na(ensembl)),], DataFrame(condition), ~ condition)
+mcols(deseqrna)$basepairs <- genelength[,1]
+
+# Counts normalized per kilobase
+normalizedRNAseq <- fpkm(deseqrna, robust = TRUE)
+
+##EXTRA
+## bp of non-normalized
+boxplot(log2(deseqrna+1), notch=TRUE,
+        main = "Non-normalized read counts",
+        ylab="log2(read counts)", cex = .6)
+## bp of size-factor normalized values
+boxplot(log2(normalizedRNAseq +1), notch=TRUE,
+        main = "Size-factor-normalized read counts",
+        ylab="log2(read counts)", cex = .6)
+
+
