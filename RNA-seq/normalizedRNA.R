99a84606f5caaca0bee8d627f74789deed1111a5
ingazalia <98822685+ingazalia@users.noreply.github.com>
2022-07-12 17:10:07 +0200

    Update normalizedRNA.R

--- RNA-seq/normalizedRNA.R
+++ RNA-seq/normalizedRNA.R
@@ -34,4 +34,57 @@ boxplot(log2(normalizedRNAseq +1), notch=TRUE,
         main = "Normalized read counts per kb",
         ylab="log2(read counts)", cex = .6)
 
+library(ggplot2)
+
+boxplotfunction <- function(ndata,n=2){
+  AD <- rowMeans(dplyr::select(ndata, starts_with("AD")))
+  old <- rowMeans(dplyr::select(ndata, starts_with("Old")))
+  if(n==3){
+    young <- rowMeans(dplyr::select(ndata, starts_with("Young")))
+    condition <- c(rep('young',length(young)),rep('old',length(old)),rep('AD',length(AD)))
+    data <- data.frame(class=rep(c('young','old','AD'),c(length(young),length(old),length(AD))),normalized_RNA=c(young,old,AD))
+    test <- kruskal.test(data$normalized_RNA~data$class)
+    name <- "Kruskal-Wallis: "
+  }else{
+    condition <- c(rep('old',length(old)),rep('AD',length(AD)))
+    data <- data.frame(class=rep(c('old','AD'),c(length(old),length(AD))),normalized_RNA=c(old,AD))
+    test <- wilcox.test(data$normalized_RNA ~ data$class)
+    name <- "Wilcox: "
+  }
+  annotations <- data.frame(
+    xpos = -Inf,
+    ypos = Inf,
+    annotateText = paste(name,signif(test$p.value,digits=3)),
+    hjustvar = 0 ,
+    vjustvar = 1) #<- adjust
+  p <- ggplot(data, aes(x=class, y=normalized_RNA),size=10) +
+    geom_boxplot()+
+    geom_label(data=annotations,aes(x=xpos,y=ypos,hjust=hjustvar,vjust=vjustvar,label=annotateText),size=6)+
+    theme_grey(base_size = 22)
+  p
+  
+}
+
+normalizedRNA <- read.table("normalizedRNA_new.txt",header=TRUE,sep = ",",row.names = 1)
+upregulatedAll <- read.table("upregulatedAll.txt",header=TRUE,row.names = 1)
+downregulatedAll <- read.table("downregulatedAll.txt",header=TRUE,row.names = 1)
+upregulated <- read.table("upregulated.txt",header=TRUE,row.names = 1)
+downregulated <- read.table("downregulated.txt",header=TRUE,row.names = 1)
+
+
+new <- normalizedRNA[upregulatedAll$genes,]
+new <- new[!is.na(new),]
+boxplotfunction(new,n=3)
+
+new <- normalizedRNA[downregulatedAll$genes,]
+new <- new[!is.na(new),]
+boxplotfunction(new,n=3)
+# old vs AD
+new <- normalizedRNA[upregulated$genes,]
+new <- new[!is.na(new),]
+boxplotfunction(new,n=2)
+
+new <- normalizedRNA[downregulated$genes,]
+new <- new[!is.na(new),]
+boxplotfunction(new,n=2)
 
