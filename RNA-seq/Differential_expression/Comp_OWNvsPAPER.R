e3a35e109ad3caf85f09c9e478b68df37d8b2cdc
ingazalia <98822685+ingazalia@users.noreply.github.com>
2022-07-21 16:10:12 +0200

    comparison own vs paper

---
+++ RNA-seq/Differential_expression/Comp_OWNvsPAPER.R
@@ -0,0 +1,68 @@
+library(readxl)
+library(devtools)
+library(ggVennDiagram)
+library(ggplot2)
+
+sheets <- excel_sheets("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx")
+resultupreg <- read_excel("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx",sheet = sheets[1])
+resultdownreg <- read_excel("RNAseq/GSE153873_AD.vs.Old_diff.genes.xlsx",sheet = sheets[2])
+upregulated <- read.table("upregulated.txt",header=TRUE)
+downregulated <- read.table("downregulated.txt",header = TRUE)
+
+# overlap of up-/downregulated genes in own results and papers results  
+length(intersect(upregulated$genes,unlist(resultupreg[,1])))
+#314 of 421
+length(intersect(downregulated$genes,unlist(resultdownreg[,1])))
+#344 of 434
+which(upregulated$genes%in%unlist(resultupreg[,1]))
+which(downregulated$genes%in%unlist(resultdownreg[,1]))
+
+# a lot of overlaps but also some up/downregulated genes but also a lot of genes missing
+# in the 50 most significant downregulated genes: overlap of 49 genes
+# in the 50 most significant upregulated genes: overlap of 45 genes
+
+x <- list(paper=unlist(resultupreg[,1]),own=upregulated$genes)
+
+venn <- Venn(x)
+data <- process_data(venn)
+png(file = "VennUpregualted.png", height = 400,width=600)
+ggplot() +
+  # 1. region count layer
+  geom_sf(aes(fill = count), data = venn_region(data),size=2) +
+  # 2. set edge layer
+  geom_sf(aes(color = name), data = venn_setedge(data), show.legend = TRUE, size = 2) +
+  # 3. set label layer
+  geom_sf_text(aes(label = name), data = venn_setlabel(data),size=5) +
+  # 4. region label layer
+  geom_sf_label(aes(label = paste0(count, " (", scales::percent(count/sum(count), accuracy = 2), ")")), 
+                data = venn_region(data),
+                size = 5) +
+   scale_fill_gradient(low = "grey80", high = "red")+
+   scale_color_manual(values = c("own" = "red","paper" ="grey70"))+
+  theme_void()
+dev.off()
+
+# description:  Venn diagramm of upregulated results paper and own (results of differential gene expression analysis).
+
+
+x <- list(paper=unlist(resultdownreg[,1]),own=downregulated$genes)
+
+venn <- Venn(x)
+data <- process_data(venn)
+png(file = "VennDownregulated.png", height = 400,width=600)
+ggplot() +
+  # 1. region count layer
+  geom_sf(aes(fill = count), data = venn_region(data),size=2) +
+  # 2. set edge layer
+  geom_sf(aes(color = name), data = venn_setedge(data), show.legend = TRUE, size = 2) +
+  # 3. set label layer
+  geom_sf_text(aes(label = name), data = venn_setlabel(data),size=5) +
+  # 4. region label layer
+  geom_sf_label(aes(label = paste0(count, " (", scales::percent(count/sum(count), accuracy = 2), ")")), 
+                data = venn_region(data),
+                size = 5) +
+  scale_fill_gradient(low = "grey80", high = "red")+
+  scale_color_manual(values = c("own" = "red","paper" ="grey70"))+
+  theme_void()
+dev.off()
+# description:  Venn diagramm of downregulated results paper and own (results of differential gene expression analysis).
